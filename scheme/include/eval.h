d8129c3fc77d1ddf755ad23a2ac03debcee715e3
Tointoin <antoine.grandry@hotmail.fr>
2016-12-05 21:25:11 +0100

    maj

--- scheme/include/eval.h
+++ scheme/include/eval.h
@@ -18,12 +18,13 @@ extern "C" {
 
 #include "object.h"
 
-object define_eval(object);
-object if_eval(object);
-object and_eval(object);
-object or_eval(object);
-object sfs_eval(object);
-object arguments_eval (object);
+object define_eval(object input,object env);
+object if_eval(object input,object env);
+object and_eval(object input,object env);
+object or_eval(object input,object env);
+object begin_eval(object input,object env);
+object sfs_eval(object input,object env);
+object arguments_eval (object input,object env);
 
 #ifdef __cplusplus
 }
